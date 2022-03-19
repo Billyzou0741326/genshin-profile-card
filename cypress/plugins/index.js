664ddd8d00be9a1b90fd8ec9da9097eaabd84e15
kotoriのねこ <2567702743@qq.com>
2022-03-19 00:48:30 +0000

    [ci] remove next plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,7 +12,7 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-const injectDevServer = require('@cypress/react/plugins/next')
+//const injectDevServer = require('@cypress/react/plugins/next')
 
 /**
  * @type {Cypress.PluginConfig}
@@ -21,7 +21,7 @@ const injectDevServer = require('@cypress/react/plugins/next')
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  injectDevServer(on, config)
+  //injectDevServer(on, config)
   config.env = process.env
   return config
 }
